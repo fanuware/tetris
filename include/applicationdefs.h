8498554784f6ee8fbcea57954c500b22b5e7b46d
fanuware <fanuware@gmail.com>
2022-01-27 16:35:26 +0100

    Update CMake

---
+++ include/applicationdefs.h
@@ -0,0 +1,11 @@
+#ifndef APPLICATIONDEFS_H
+#define APPLICATIONDEFS_H
+
+#include <QString>
+
+namespace ApplicationDefs {
+    extern const QString kVersion;
+    extern const QString kLegalCopyright;
+}
+
+#endif
--- src/about.cpp
+++ src/about.cpp
@@ -1,5 +1,6 @@
 #include "about.h"
 #include "ui_about.h"
+#include "applicationdefs.h"
 #include <QtCore>
 #include <QtGui>
 
@@ -10,6 +11,9 @@ About::About(QWidget *parent) :
 	ui->setupUi(this);
 	window()->layout()->setSizeConstraint(QLayout::SetFixedSize);
 	setWindowIcon(QPixmap(":/res/tetris_icon.png"));
+
+	ui->versionLabel->setText(ApplicationDefs::kVersion);
+	ui->legalCopyrightLabel->setText(ApplicationDefs::kLegalCopyright);
 
 	// connections
 	connect(ui->okButton, &QPushButton::clicked, this, &About::close);
